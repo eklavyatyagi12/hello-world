026ee7e496e7c5a6f51e740332046fd07475dd86
eklavya <64527842+eklavyatyagi12@users.noreply.github.com>
2020-09-29 14:17:43 +0530

    Add files via upload
    adding elements to list frrom a text docu and working with it

---
+++ project.py
@@ -0,0 +1,12 @@
+fname = input("Enter file name: ")
+count = 0
+fh = open(fname)
+
+for line in fh :
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    words = line.split()
+    print(words[1])
+    count +=1
+
+print ("There were", count, "lines in the file with From as the first word")
